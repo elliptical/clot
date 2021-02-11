6672a519ab3ea722f357ec554da6911741dcec8b
Andrei Boulgakov <andrei.boulgakov@outlook.com>
2021-02-10 22:27:17 -0800

    Fix reading version from a code file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Run "python setup.py install" to install the bencode package.
 """
 
 
-import os
+from os import path
 import re
 import sys
 
@@ -20,12 +20,18 @@ if sys.version_info < (3, 6):
 
 def get_version():
     """Return the version string from the package's __init__.py file."""
-    with open(os.path.join(PACKAGE_NAME, '__init__.py')) as version_file:
+    with open_text_file(PACKAGE_NAME, '__init__.py') as version_file:
         version_source = version_file.read()
     version_match = re.search(r"^__version__ = '([^']*)'", version_source, re.MULTILINE)
     if not version_match:
         raise RuntimeError('Could not find the version string.')
     return version_match.group(1)
+
+
+def open_text_file(*path_segments):
+    """Open the specified UTF-8 encoded file relative to setup.py location."""
+    my_dir = path.abspath(path.dirname(__file__))
+    return open(path.join(my_dir, *path_segments), encoding='utf-8')
 
 
 VERSION = get_version()
