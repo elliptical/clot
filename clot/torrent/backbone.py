c2ebb04cbf5ee41750c2d496c160370b6ac5efcd
Andrei Boulgakov <andrei.boulgakov@outlook.com>
2021-02-25 21:57:34 -0800

    Add the dump() function

--- clot/torrent/backbone.py
+++ clot/torrent/backbone.py
@@ -1,7 +1,19 @@
 """This module implements the torrent's underlying storage."""
 
 
+import json
+
 from .. import bencode
+
+
+class _JsonEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, bytes):
+            try:
+                return o.decode()
+            except UnicodeDecodeError:
+                return 'hex::' + o.hex()
+        return super().default(o)
 
 
 class Backbone:
@@ -32,3 +44,9 @@ class Backbone:
         with open(self.file_path, 'wb') as file:
             file.write(raw_bytes)
         self.raw_bytes = raw_bytes
+
+    def dump(self, file_path, *, indent=None, sort_keys=False, overwrite=False):
+        """Write the torrent to a file in JSON format."""
+        with open(file_path, 'w' if overwrite else 'x', encoding='utf-8') as file:
+            json.dump(self.data, file, cls=_JsonEncoder, ensure_ascii=False,
+                      indent=indent, sort_keys=sort_keys)
--- test/torrent/test_backbone_save.py
+++ test/torrent/test_backbone_save.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from os import path, remove
 from tempfile import NamedTemporaryFile
+import textwrap
 
 import tcm
 
@@ -79,9 +80,100 @@ class SaveTestCase(tcm.TestCase):
             self.assertEqual(read_bytes(file_path), b'd2:my5:valuee')
 
 
+class DumpTestCase(tcm.TestCase):
+    def test_existing_file_will_raise_by_default(self):
+        t = torrent.new()
+
+        with temp_file_path(contents=b'old garbage') as file_path:
+            self.assertTrue(path.exists(file_path))
+            self.assertEqual(read_bytes(file_path), b'old garbage')
+
+            with self.assertRaises(FileExistsError):
+                t.dump(file_path)
+            self.assertEqual(read_bytes(file_path), b'old garbage')
+
+    def test_existing_file_can_be_overwritten_when_asked(self):
+        t = torrent.new()
+
+        with temp_file_path(contents=b'old garbage') as file_path:
+            self.assertTrue(path.exists(file_path))
+            self.assertEqual(read_bytes(file_path), b'old garbage')
+
+            t.dump(file_path, overwrite=True)
+            self.assertEqual(read_str(file_path), '{}')
+
+    def test_default_is_oneline_unsorted(self):
+        t = torrent.new()
+        t.data['my'] = 'value'
+        t.data['many'] = [-1, 'two', {'x': 1, 'y': 2}]
+        expected_json = '{"my": "value", "many": [-1, "two", {"x": 1, "y": 2}]}'
+
+        with temp_file_path(suffix='.json') as file_path:
+            self.assertFalse(path.exists(file_path))
+
+            t.dump(file_path)
+            self.assertEqual(t.raw_bytes, b'de')
+            self.assertIsNone(t.file_path)
+            self.assertTrue(path.exists(file_path))
+            self.assertEqual(read_str(file_path), expected_json)
+
+    def test_output_can_be_multiline_indented_sorted(self):
+        t = torrent.new()
+        t.data['my'] = 'value'
+        t.data['many'] = [-1, 'two', {'x': 1, 'y': 2}]
+        expected_json = textwrap.dedent("""
+            {
+              "many": [
+                -1,
+                "two",
+                {
+                  "x": 1,
+                  "y": 2
+                }
+              ],
+              "my": "value"
+            }
+            """).strip()
+
+        with temp_file_path(suffix='.json') as file_path:
+            self.assertFalse(path.exists(file_path))
+
+            t.dump(file_path, indent=2, sort_keys=True)
+            self.assertEqual(t.raw_bytes, b'de')
+            self.assertIsNone(t.file_path)
+            self.assertTrue(path.exists(file_path))
+            self.assertEqual(read_str(file_path), expected_json)
+
+    def test_bytes_output_as_string(self):
+        t = torrent.new()
+        t.data['x'] = b'value'      # plain ASCII
+        t.data['y'] = b'\xab\xcd'   # not a valid UTF-8, will be hex in the output
+        t.data['z'] = '\N{SHRUG}'   # 4-byte UTF-8
+        expected_json = '{"x": "value", "y": "hex::abcd", "z": "' + '\N{SHRUG}' + '"}'
+
+        with temp_file_path(suffix='.json') as file_path:
+            self.assertFalse(path.exists(file_path))
+
+            t.dump(file_path)
+            self.assertEqual(t.raw_bytes, b'de')
+            self.assertIsNone(t.file_path)
+            self.assertTrue(path.exists(file_path))
+            self.assertEqual(read_str(file_path), expected_json)
+
+    def test_unexpected_types_will_raise(self):
+        t = torrent.new()
+        t.data['x'] = self
+
+        with temp_file_path(suffix='.json') as file_path:
+            with self.assertRaises(TypeError) as outcome:
+                t.dump(file_path)
+            message = outcome.exception.args[0]
+            self.assertIn('not JSON serializable', message)
+
+
 @contextmanager
-def temp_file_path(*, contents=None):
-    with NamedTemporaryFile(prefix='clot-', suffix='.torrent', delete=contents is None) as file:
+def temp_file_path(*, suffix='.torrent', contents=None):
+    with NamedTemporaryFile(prefix='clot-', suffix=suffix, delete=contents is None) as file:
         if contents is not None:
             file.write(contents)
         file_path = file.name
@@ -94,4 +186,9 @@ def temp_file_path(*, contents=None):
 
 def read_bytes(file_path):
     with open(file_path, 'rb') as file:
+        return file.read()
+
+
+def read_str(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
