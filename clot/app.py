d34cf41a9988331d7961704c697301e346da138e
Andrei Boulgakov <andrei.boulgakov@outlook.com>
2021-02-26 18:33:15 -0800

    Specify input files and directories

--- clot/app.py
+++ clot/app.py
@@ -2,13 +2,23 @@
 
 
 from argparse import ArgumentParser
+from os import path, walk
 
 from clot import __version__
 
 
 def main():
     """Execute actions according to the command-line arguments."""
-    _parse_command_line()
+    args = _parse_command_line()
+
+    for source in args.sources:
+        if path.isdir(source):
+            traverse_dir(source, args)
+        elif path.isfile(source):
+            handle_file(source, args)
+        else:
+            if args.verbose:
+                print(f'Could not locate file or directory "{source}".')
 
 
 def _parse_command_line():
@@ -19,7 +29,53 @@ def _parse_command_line():
                         action='version',
                         version=f'{__file__} {__version__}')
 
-    parser.parse_args()
+    _add_traversal_arguments_to(parser)
+
+    return parser.parse_args()
+
+
+def _add_traversal_arguments_to(parser):
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        help='print additional information')
+
+    parser.add_argument('-r', '--recurse',
+                        action='store_true',
+                        help='recurse into subdirectories')
+
+    parser.add_argument('--ext',
+                        default='.torrent',
+                        help='filter the directories based on filename extension'
+                             ' (default: "%(default)s")')
+
+    parser.add_argument('sources',
+                        nargs='*',
+                        metavar='PATH',
+                        default='.',
+                        help='torrent file or directory with torrent files'
+                             ' (default: current directory)')
+
+
+def traverse_dir(dir_path, args):
+    """Traverse the directory (flat or recursive) and handle files with the specified extension."""
+    for root, dirs, files in walk(dir_path):
+        if not args.recurse:
+            dirs.clear()
+
+        for name in files:
+            if name.endswith(args.ext):
+                file_path = path.join(root, name)
+                handle_file(file_path, args)
+
+
+def handle_file(file_path, args):
+    """Handle the specified file based on args."""
+    if args.verbose:
+        print(file_path)
+
+    # For now, just ensure we can read the file.
+    with open(file_path, 'rb') as file:
+        file.read()
 
 
 if __name__ == '__main__':
