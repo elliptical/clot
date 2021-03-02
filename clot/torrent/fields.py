c7fa4a4d3ff16c7b688343af3ca9148f4286cc92
Andrei Boulgakov <andrei.boulgakov@outlook.com>
2021-03-01 19:29:59 -0800

    Add descriptor for field values

---
+++ clot/torrent/fields.py
@@ -0,0 +1,50 @@
+"""This module implements a descriptor to handle torrent fields."""
+
+
+class Field:
+    """Field with specific type and unrestricted values, including None."""
+
+    def __init__(self, key, value_type):
+        """Initialize self."""
+        self.key = key
+        self.value_type = value_type
+
+    def __set_name__(self, owner, name):
+        """Customize the name used to store the field value."""
+        # pylint: disable=attribute-defined-outside-init
+        self.name = name
+        self.private_name = '_' + name
+
+    def __get__(self, instance, owner=None):
+        """Return the field value from the specified instance (or the descriptor itself)."""
+        if instance is None:
+            return self
+
+        try:
+            return getattr(instance, self.private_name)
+        except AttributeError:
+            return self.load_from(instance)
+
+    def __set__(self, instance, value):
+        """Set the field value in the specified instance."""
+        if value is not None:
+            self.validate(value)
+        setattr(instance, self.private_name, value)
+
+    def load_from(self, instance):
+        """Initialize the field value from the instance data dictionary."""
+        try:
+            value = instance.data[self.key]
+        except KeyError:
+            value = None
+        else:
+            self.validate(value)
+            del instance.data[self.key]
+
+        setattr(instance, self.private_name, value)
+        return value
+
+    def validate(self, value):
+        """Raise an exception on unexpected value type."""
+        if not isinstance(value, self.value_type):
+            raise TypeError(f'{self.name}: expected {value!r} to be of type {self.value_type}')
---
+++ test/torrent/test_fields.py
@@ -0,0 +1,90 @@
+import tcm
+
+from clot.torrent.fields import Field
+
+
+class Base:
+    def __init__(self, **kwargs):
+        self.data = kwargs
+
+
+class FieldTestCase(tcm.TestCase):
+    def test_property_itself_is_accessible(self):
+        class Dummy(Base):
+            field = Field('x', int)
+
+        self.assertIsInstance(Dummy.field, Field)
+
+    def test_missing_field_is_none(self):
+        class Dummy(Base):
+            field = Field('x', int)
+
+        dummy = Dummy(z=3)
+
+        self.assertFalse(hasattr(dummy, '_field'))
+        value = dummy.field
+        self.assertTrue(hasattr(dummy, '_field'))
+
+        self.assertIsNone(value)
+        self.assertDictEqual(dummy.data, {'z': 3})
+
+    def test_present_field_is_auto_loaded(self):
+        class Dummy(Base):
+            field = Field('x', int)
+
+        dummy = Dummy(x=1)
+
+        self.assertFalse(hasattr(dummy, '_field'))
+        value = dummy.field
+        self.assertTrue(hasattr(dummy, '_field'))
+
+        self.assertEqual(value, 1)
+        self.assertDictEqual(dummy.data, {})
+
+    def test_auto_load_will_raise_on_bad_value(self):
+        class Dummy(Base):
+            field = Field('x', int)
+
+        dummy = Dummy(x='1')
+
+        self.assertFalse(hasattr(dummy, '_field'))
+
+        with self.assertRaises(TypeError) as outcome:
+            _ = dummy.field
+        message = outcome.exception.args[0]
+        self.assertEqual(message, "field: expected '1' to be of type <class 'int'>")
+
+        self.assertFalse(hasattr(dummy, '_field'))
+        self.assertDictEqual(dummy.data, {'x': '1'})
+
+    def test_data_is_not_popped_when_field_is_set(self):
+        class Dummy(Base):
+            field = Field('x', int)
+
+        dummy = Dummy(x=1)
+
+        self.assertFalse(hasattr(dummy, '_field'))
+        dummy.field = 0
+        self.assertTrue(hasattr(dummy, '_field'))
+
+        self.assertEqual(dummy.field, 0)
+        self.assertDictEqual(dummy.data, {'x': 1})
+
+    @tcm.values(
+        (int,   123,    '123'),
+        (str,   '123',  123),
+    )
+    def test_value_type_is_enforced(self, value_type, good_value, bad_value):
+        class Dummy(Base):
+            field = Field('x', value_type)
+
+        dummy = Dummy()
+        dummy.field = good_value
+        self.assertEqual(dummy.field, good_value)
+        dummy.field = None
+        self.assertIsNone(dummy.field)
+
+        with self.assertRaises(TypeError) as outcome:
+            dummy.field = bad_value
+        message = outcome.exception.args[0]
+        self.assertIn(f'of type {value_type}', message)
